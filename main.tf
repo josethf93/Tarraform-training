b96f0d7688cf0cb24cd50c04c57da5dc099c9157
josethf93 <joseth.f93@gmail.com>
2021-04-05 21:23:03 -0700

    modified variable.tf adding a list variable

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 resource "aws_instance" "joseth_ec2" {
-    ami = var.amiid
-    instance_type = "t2.micro"
-    tags = {
-      "Name" = "joseth4/6"
-    }
+  count         = var.instanceCount
+  ami           = var.amiid
+  instance_type = "t2.micro"
+  tags = {
+    # "Name" = "Joseth_EC2_${count.index + 1}"
+    "Name" = var.tags[count.index]
+  }
 }
 
 output "joseth_ec2ipaddress" {
-    value = aws_instance.joseth_ec2.public_ip
+  value = aws_instance.joseth_ec2[0].public_ip
 }
 
 output "joseth_ec2privateipaddress" {
-    value = aws_instance.joseth_ec2.private_ip
+  value = aws_instance.joseth_ec2[*].private_ip
 }
